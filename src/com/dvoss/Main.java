4f7e071b9e25e5f4ed5a35fc780df2c6c0245e96
Dan <danielandrewvoss@gmail.com>
2016-11-18 11:18:58 -0500

    Initial import.

---
+++ src/com/dvoss/Main.java
@@ -0,0 +1,16 @@
+package com.dvoss;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class Main {
+
+    public static void main(String[] args) throws TwitterException {
+
+        Twitter twitter = new TwitterFactory().getSingleton();
+        Status status = twitter.updateStatus("another try.");
+        System.out.println("Tweeted successful-lee.");
+    }
+}
