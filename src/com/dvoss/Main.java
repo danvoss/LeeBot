f8f78f62fd1abd16d5a8ef0fa21b4405c1381908
Dan <danielandrewvoss@gmail.com>
2016-11-18 12:54:53 -0500

    Add query

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -1,16 +1,26 @@
 package com.dvoss;
 
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 
 public class Main {
 
     public static void main(String[] args) throws TwitterException {
 
         Twitter twitter = new TwitterFactory().getSingleton();
-        Status status = twitter.updateStatus("i tweet robotic-lee.");
-        System.out.println("Tweeted successful-lee.");
+
+//        Status status = twitter.updateStatus("i tweet robotic-lee.");
+//        System.out.println("Tweeted successful-lee.");
+
+        Query query = new Query("lee konitz");
+
+        QueryResult result = twitter.search(query);
+
+        Status found = result.getTweets().get(0);
+
+        System.out.println(found.getText());
+
+        /* for retweet, grab id
+         * problem: avoid retweeting the same status more than once
+         */
     }
 }
