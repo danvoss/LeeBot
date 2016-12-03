ce02d4d407f92dc60a7a3df93b94ea56ceae0d3a
Dan <danielandrewvoss@gmail.com>
2016-12-03 15:26:45 -0500

    Create while loop to search and retweet

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -4,23 +4,33 @@ import twitter4j.*;
 
 public class Main {
 
-    public static void main(String[] args) throws TwitterException {
+    public static void main(String[] args) throws TwitterException, InterruptedException {
 
         Twitter twitter = new TwitterFactory().getSingleton();
+
+        /* first test-drive:
+        * */
 
 //        Status status = twitter.updateStatus("i tweet robotic-lee.");
 //        System.out.println("Tweeted successful-lee.");
 
-        Query query = new Query("lee konitz");
+        /* run script to search for latest tweets and retweet:
+        * */
 
-        QueryResult result = twitter.search(query);
+        while (true) {
 
-        Status found = result.getTweets().get(0);
+            Query query = new Query("lee konitz");
 
-        System.out.println(found.getText());
+            QueryResult result = twitter.search(query);
 
-        /* for retweet, grab id
-         * problem: avoid retweeting the same status more than once
-         */
+            for (int i = 27; i >= 0; i--) {
+                Status found = result.getTweets().get(i);
+//                twitter.retweetStatus(found.getId());
+                System.out.println(found.getText());
+                Thread.sleep(60 * 1000);
+            }
+
+            Thread.sleep((6 * 60 * 60 * 1000));
+        }
     }
 }
